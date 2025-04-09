767d0f5209de5385f17505ef915f58064cf7da19
Manar-202 <119630678+Manar-202@users.noreply.github.com>
2025-04-09 22:57:02 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import ImagesSlider from './components/common/ImagesSlider.jsx'
-import OverviewCmp from './components/common/overviewCmp.jsx'
+
 function App() {
   return (
     <>
-    <ImagesSlider/>
     
-    <OverviewCmp/>
+    
+    
   
     </>
   );
