eb59d436a1bd8366665dfb73119bcb433aef6d4e
Manar-202 <119630678+Manar-202@users.noreply.github.com>
2025-04-13 21:52:13 +0200

    all

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import NavBar from './components/layout/NavBar'
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import ComparePage from './components/pages/ComparePage';
 import PropertiesGridPage from './components/pages/PropertiesGridPage';
-import { FavoriteProvider } from './components/common/FavoriteContext'; 
+import { FavoriteProvider } from './components/common/FavoriteContext';
 import { CompareProvider } from './components/common/CompareContext';
 import Footer from './components/layout/Footer'
 import PropertySingle from './components/pages/PropertySingle'
@@ -23,45 +23,51 @@ import Compare from "./components/pages/Compare.jsx";
 
 
 import MyFavorites from "./components/pages/MyFavorites.jsx";
+//import { Elements } from '@stripe/react-stripe-js';
+//import { loadStripe } from '@stripe/stripe-js';
+//const stripePromise = loadStripe('pk_test_51RC3rdGavfGkHWZgLjk33Vfew5sg4nqKbBuoRt5gg2z4Unr7M3mrUpiRGNKkbA2wwdSwBCJJSw9IX1hLCwTXASXP00kwvlxaEH');
+
+
+// import MyFavorites from "./components/pages/MyFavorites.jsx";
 
 function App() {
-  const theme = createTheme({
-    components: {
-      MuiSvgIcon: {
-        styleOverrides: {
-          root: {
-            color: '#1cb3ff', 
-          },
+    const theme = createTheme({
+        components: {
+            MuiSvgIcon: {
+                styleOverrides: {
+                    root: {
+                        color: '#1cb3ff',
+                    },
+                },
+            },
         },
-      },
-    },
-  });
-  
-  return (
-    <ThemeProvider theme={theme}>
-    <CompareProvider>
-    <FavoriteProvider>
-    <Router>
-      <div className="min-h-screen flex flex-col">
-        <NavBar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/properties" element={<PropertiesGridPage/>} />
-            <Route path="/property/:id" element={<PropertySingle />} />
-            <Route path="/compared" element={<ComparePage />} />
-            {/* <Route path="/property-single" element={<PropertySingle />} />
+    });
+
+    return (
+        <ThemeProvider theme={theme}>
+            <CompareProvider>
+                <FavoriteProvider>
+                    <Router>
+                        <div className="min-h-screen flex flex-col">
+                            <NavBar />
+                            <main className="flex-grow">
+                                <Routes>
+                                    <Route path="/" element={<Home />} />
+                                    <Route path="/dashboard" element={<Dashboard />} />
+                                    <Route path="/properties" element={<PropertiesGridPage />} />
+                                    <Route path="/property/:id" element={<PropertySingle />} />
+                                    <Route path="/compared" element={<ComparePage />} />
+                                    {/* <Route path="/property-single" element={<PropertySingle />} />
             <Route path="/contact" element={<Contact />} /> */}
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
-    </FavoriteProvider>
-    </CompareProvider>
-    </ThemeProvider>
-  );
+                                </Routes>
+                            </main>
+                            <Footer />
+                        </div>
+                    </Router>
+                </FavoriteProvider>
+            </CompareProvider>
+        </ThemeProvider>
+    );
 }
 
 export default App;
